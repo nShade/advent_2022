3aab090d2214294bf8b72594bb85b292aded1067
nShade <nshade@fastmail.com>
2022-12-06 21:01:47 +0100

    ADD day 3 and day 4

--- day_3/part_2.py
+++ day_3/part_2.py
@@ -1,4 +1,23 @@
+import string
+from itertools import zip_longest
+
+PRIORITY = dict(**{x: ord(x) - 96 for x in string.ascii_lowercase}, **{x: ord(x) - 38 for x in string.ascii_uppercase})
+
+
+def similar_item(a, b, c):
+    return (set(a) & set(b) & set(c)).pop()
+
+
+def convert_line(line):
+    return [PRIORITY[sym] for sym in line]
+
+
 if __name__ == "__main__":
     with open("input", "r") as text_input:
         input_lines = text_input.readlines()
-        print("I don't know")
+
+    converted_list = [convert_line(line.replace('\n', '')) for line in input_lines]
+    chunked_list = list(zip_longest(*[converted_list[i::3] for i in range(3)]))
+    res = sum([similar_item(*item) for item in chunked_list])
+    print(res)
+
