cc9cbd9fc27f2652ed59b740277fc0158f2d4ff9
nShade <nshade@fastmail.com>
2022-12-10 09:35:53 +0100

    UPDATE Day 10 simplified, part 2 solved

--- day_10/main.py
+++ day_10/main.py
@@ -1,51 +1,26 @@
-import itertools
-
-
-def process_command(line):
-    if line[:4] == 'noop':
-        return 'noop', None
-    else:
-        return 'addx', int(line[5:])
-
-
-def execute_command(command, register):
-    cmd, arg = command
-    if cmd == "noop":
-        return register
-    if cmd == "addx":
-        return register + arg
-    return register
-
-
-def run_cycle(command_cache, input_lines, register):
-    if command_cache:
-        command = command_cache
-        command_cache = None
-    else:
-        command = process_command(next(input_lines))
-        if command[0] == 'addx':
-            command_cache = command
-            command = None, None
-
-    register = execute_command(command, register)
-    return command_cache, register
-
-
 def run_cycles(input_lines):
-    command_cache = None
     register = 1
-    while True:
-        try:
-            command_cache, register = run_cycle(command_cache, input_lines, register)
+
+    for line in input_lines:
+        yield register
+
+        if line.startswith('addx'):
             yield register
-        except StopIteration:
-            return
+            register += int(line[5:])
 
 
 if __name__ == "__main__":
     with open("input", "r") as input_file:
-        input_lines = iter(input_file.readlines())
+        input_lines = input_file.readlines()
 
     registers = list(run_cycles(input_lines))
-    res_1 = sum([registers[cycle - 2] * cycle for cycle in range(20, 240, 40)])
+    res_1 = sum([registers[cycle - 1] * cycle for cycle in range(20, 240, 40)])
     print(f"Part 1: {res_1}")
+    screen = ["#"
+              if register - 1 <= pixel % 40 <= register + 1
+              else "."
+              for pixel, register in enumerate(registers)]
+    res_2 = '\n'.join([''.join(screen[i * 40:(i + 1) * 40]) for i in range(6)])
+    print("Part 2:")
+    print(res_2)
+   
