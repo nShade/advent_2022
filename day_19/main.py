c6187e0fc776c6b74a27f2f447b0f38a67867ba1
nShade <nshade@fastmail.com>
2022-12-10 21:28:31 +0100

    ADD day 11 to 24 templates

---
+++ day_19/main.py
@@ -0,0 +1,9 @@
+if __name__ == "__main__":
+    with open("input", "r") as input_file:
+        input_lines = input_file.readlines()
+
+    res_1 = "IDK"
+    print(f"Part 1: {res_1}")
+    res_2 = "IDK"
+    print(f"Part 2: {res_2}")
+   
