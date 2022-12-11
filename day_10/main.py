ca69b43cdebd98f6c94f85f480004548cba52c5a
nShade <nshade@fastmail.com>
2022-12-11 11:17:20 +0100

    UPDATE Day 11 part 1 not solved

--- day_10/main.py
+++ day_10/main.py
@@ -18,9 +18,8 @@ if __name__ == "__main__":
     print(f"Part 1: {res_1}")
     screen = ["#"
               if register - 1 <= pixel % 40 <= register + 1
-              else "."
+              else " "
               for pixel, register in enumerate(registers)]
     res_2 = '\n'.join([''.join(screen[i * 40:(i + 1) * 40]) for i in range(6)])
     print("Part 2:")
     print(res_2)
-   
--- day_11/main.py
+++ day_11/main.py
@@ -1,9 +1,53 @@
+import re
+
+MONKEY_PATTERN = re.compile(r"""Monkey (?P<monkey>\d):
+  Starting items: (?P<items>(?:\d+, )*\d+)
+  Operation: new = (?P<operation>.*)
+  Test: divisible by (?P<test>\d+)
+    If true: throw to monkey (?P<test_positive_throw>\d)
+    If false: throw to monkey (?P<test_negative_throw>\d)""")
+
+
+def monkey_test(item, operation, test):
+    """
+    >>> monkey_test(44, "old - 2", 3)
+    (14, False)
+    """
+    new = round(eval(operation, {}, {'old': item}) / 3)
+    return new, new % test == 0
+
+
+def monkey_turn(items, operation, test):
+    for item in items:
+        yield monkey_test(item, operation, test)
+
+
+def monkey_round(monkeys):
+    for monkey_id in range(8):
+        monkey = monkeys[monkey_id]
+        yield len(monkey['items'])
+
+        for item, test_result in monkey_turn(monkey['items'], monkey['operation'], monkey['test']):
+            monkeys[monkey['throw'][test_result]]['items'].append(item)
+
+        monkey['items'] = []
+
+
 if __name__ == "__main__":
     with open("input", "r") as input_file:
-        input_lines = input_file.readlines()
+        input_text = input_file.read()
 
-    res_1 = "IDK"
-    print(f"Part 1: {res_1}")
+    monkeys = {int(monkey_dict['monkey']): {'items': [int(item) for item in monkey_dict['items'].split(", ")],
+                                            'test': int(monkey_dict['test']),
+                                            'operation': monkey_dict['operation'],
+                                            'throw': {True: int(monkey_dict['test_positive_throw']),
+                                                      False: int(monkey_dict['test_negative_throw'])}}
+               for monkey_dict in MONKEY_PATTERN.finditer(input_text)}
+
+    inspected = list(map(sum, zip(*[list(monkey_round(monkeys)) for i in range(20)])))
+    two_most_active = sorted(inspected)[-2:]
+    monkey_business = two_most_active[0] * two_most_active[1]
+
+    print(f"Part 1: {monkey_business}")
     res_2 = "IDK"
     print(f"Part 2: {res_2}")
-   
