2501e6d990e36f4670b38407646a6564e416d109
nShade <nshade@fastmail.com>
2022-12-11 18:15:42 +0100

    UPDATE Day 11 significant speed up

--- day_11/main.py
+++ day_11/main.py
@@ -11,25 +11,22 @@ MONKEY_PATTERN = re.compile(r"""Monkey (?P<monkey>\d):
     If false: throw to monkey (?P<test_negative_throw>\d)""")
 
 
-def monkey_test(operation, test, throw, manage, item):
-    new = manage(eval(operation, {}, {'old': item}))
-    return new, throw[new % test == 0]
-
-
-def monkey_round(monkeys, manage):
-    for i, (items, test) in enumerate(monkeys):
-        yield len(items)
-
-        for item in items:
-            item, throw_to = test(manage, item)
-            monkeys[throw_to][0] += [item]
-
-        monkeys[i][0] = []
+def monkey_test(operation, test, positive_throw, negative_throw, manage, item):
+    new = manage(operation(item))
+    return new, positive_throw if new % test == 0 else negative_throw
 
 
 def calculate_monkey_business(monkeys, manage, rounds):
-    res = [monkey_round(monkeys, manage) for i in range(rounds)]
-    inspected = list(map(sum, zip(*list(map(list, res)))))
+    inspected = [0 for i in range(len(monkeys))]
+    for round in range(rounds):
+        for i, (items, test) in enumerate(monkeys):
+            for item in items:
+                item, throw_to = test(manage, item)
+                monkeys[throw_to][0].append(item)
+                inspected[i] += 1
+
+            monkeys[i][0] = []
+
     two_most_active = sorted(inspected)[-2:]
     return two_most_active[0] * two_most_active[1]
 
@@ -41,10 +38,10 @@ if __name__ == "__main__":
     parsed_text = [monkey_dict for monkey_dict in MONKEY_PATTERN.finditer(input_text)]
     monkeys = [[[int(item) for item in monkey_dict['items'].split(", ")],
                 functools.partial(monkey_test,
-                                  monkey_dict['operation'],
+                                  eval("lambda old: " + monkey_dict['operation']),
                                   int(monkey_dict['test']),
-                                  {True: int(monkey_dict['test_positive_throw']),
-                                   False: int(monkey_dict['test_negative_throw'])})]
+                                  int(monkey_dict['test_positive_throw']),
+                                  int(monkey_dict['test_negative_throw']))]
                for monkey_dict in parsed_text]
     monkeys_2 = copy.deepcopy(monkeys)
 
