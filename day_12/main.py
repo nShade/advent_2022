11c1d012ff49a0b89691d05cdeeb2e18e2fdb5ea
nShade <nshade@fastmail.com>
2022-12-14 09:15:33 +0100

    UPDATE Day 12 improved significantly

--- day_12/main.py
+++ day_12/main.py
@@ -15,63 +15,24 @@ def is_path_exists(sym_from, sym_to):
     return elevation(sym_to) - elevation(sym_from) < 2
 
 
-def find_path(solutions, paths, start, end, moves=0):
-    start_y, start_x = start
-
-    if start == end:
-        return 0
-
-    if solutions[start_y][start_x] is not None and solutions[start_y][start_x] <= moves:
-        return None
-
-    solutions[start_y][start_x] = moves
-    is_path_right, is_path_left, is_path_down, is_path_up = paths[start_y][start_x]
-
-    path_r, path_l, path_d, path_u = None, None, None, None
-
-    if is_path_right:
-        try:
-            path_r = find_path(solutions, paths, (start_y, start_x + 1), end, moves + 1)
-        except:
-            pass
-    if is_path_left:
-        try:
-            path_l = find_path(solutions, paths, (start_y, start_x - 1), end, moves + 1)
-        except:
-            pass
-    if is_path_down:
-        try:
-            path_d = find_path(solutions, paths, (start_y + 1, start_x), end, moves + 1)
-        except:
-            pass
-    if is_path_up:
-        try:
-            path_u = find_path(solutions, paths, (start_y - 1, start_x), end, moves + 1)
-        except:
-            pass
-
-    if path_u == path_d == path_r == path_l is None:
-        return None
-
-    res = min([i for i in (path_r, path_l, path_d, path_u) if i is not None]) + 1
-    return res
+def construct_paths(links, end, start, visited=set()):
+    next_locations = {l for location in start for l in links[location] if l not in visited}
+    if end in next_locations:
+        return 1
+    return 1 + construct_paths(links, end, next_locations, visited | next_locations)
 
 
 if __name__ == "__main__":
     with open("input", "r") as input_file:
         input_lines = input_file.readlines()
 
-    width = len(input_lines[0]) - 1
-    height = len(input_lines)
-
-    paths = ()
+    paths = {}
     possible_hike_starts = ()
     start, end = None, None
     for i, (line, line_below, line_above) \
             in enumerate(zip(input_lines,
                              input_lines[1:] + ["~" * len(input_lines[0])],
                              ["~" * len(input_lines[0])] + input_lines[:-1])):
-        p = ()
         for j, (symbol, symbol_right, symbol_left, symbol_below, symbol_above) \
                 in enumerate(zip(line[:-1],
                                  line[1:],
@@ -84,26 +45,19 @@ if __name__ == "__main__":
                 end = (i, j)
             if symbol == 'a':
                 possible_hike_starts += ((i, j),)
-            p += ((is_path_exists(symbol, symbol_right),
-                   is_path_exists(symbol, symbol_left),
-                   is_path_exists(symbol, symbol_below),
-                   is_path_exists(symbol, symbol_above)),)
 
-        paths += (p,)
+            location_paths = ()
+            if is_path_exists(symbol, symbol_right):
+                location_paths += ((i, j + 1),)
+            if is_path_exists(symbol, symbol_left):
+                location_paths += ((i, j - 1),)
+            if is_path_exists(symbol, symbol_below):
+                location_paths += ((i + 1, j),)
+            if is_path_exists(symbol, symbol_above):
+                location_paths += ((i - 1, j),)
+            paths[(i, j)] = location_paths
 
-    solutions = [[None for i in range(width)] for j in range(height)]
-
-    res_1 = find_path(solutions, paths, start, end)
+    res_1 = construct_paths(paths, end, {start}, {start})
     print(f"Part 1: {res_1}")
-
-    res_2 = res_1
-    for start in possible_hike_starts:
-        solutions = [[None for i in range(width)] for j in range(height)]
-        try:
-            res = find_path(solutions, paths, start, end)
-            if res:
-                res_2 = min(res, res_2)
-        except:
-            pass
-
+    res_2 = construct_paths(paths, end, set(possible_hike_starts), set(possible_hike_starts))
     print(f"Part 2: {res_2}")
